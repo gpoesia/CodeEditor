4590ee500350e65a976bd9b8d06c56ad25f3d6b8
Gabriel Poesia <gabriel.poesia@gmail.com>
2016-12-13 15:47:18 -0200

    build.sh: remove client dependency on jsx.
    Bump version to 1.0.1.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+jsx --version &> /dev/null
+if [ $? -ne 0 ]
+then
+    echo "jsx not found! Installing react-tools..."
+    npm install -g react-tools
+fi
+
+echo "Building release directory..."
+jsx src/ release/
